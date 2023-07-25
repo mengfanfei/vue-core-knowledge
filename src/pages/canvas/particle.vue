37a699e4fb3b09d3cd157c488a6f40d7d44fbbe7
mengfanfei <15863882035@163.com>
2023-07-25 23:06:32 +0800

    增加canvas

--- src/pages/canvas/particle.vue
+++ src/pages/canvas/particle.vue
@@ -6,34 +6,73 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 
-onMounted(() => {
-  drawParticle()
-})
+let canvas: HTMLCanvasElement, context: CanvasRenderingContext2D
 
 /** 
  * 创建粒子
  */
-const dots = []
-for (let i = 0; i < 100; i++) {
+const dots: any[] = []
+for (let i = 0; i < 150; i++) {
   dots.push({
     x: Math.random() * document.body.clientWidth, // x  , y  为  粒子坐标
     y: Math.random() * document.body.clientHeight,
-    xa: Math.random() * 3 - 1, // xa , ya 为  粒子 xy 轴加速度
-    ya: Math.random() * 3 - 1,
-    max: 100 // 连线的最大距离 px
+    xa: Math.random() * 2 - 1, // xa , ya 为  粒子 xy 轴加速度
+    ya: Math.random() * 2 - 1,
+    max: 150 // 连线的最大距离 px
   })
 }
 
-
+/**
+ * 绘制粒子
+ */
 const drawParticle = () => {
-  const canvas = document.getElementById('particle') as HTMLCanvasElement
-  const context = canvas.getContext('2d') as CanvasRenderingContext2D
+  canvas = document.getElementById('particle') as HTMLCanvasElement
+  context = canvas.getContext('2d') as CanvasRenderingContext2D
   canvas.width = document.body.clientWidth
   canvas.height = document.body.clientHeight
 
   context.clearRect(0, 0, canvas.width, canvas.height)
-  context.fillStyle = 'rgba(255,255,0, 1)'
+  context.fillStyle = 'rgba(0, 43, 54, 1)'
   context.fillRect(0, 0, canvas.width, canvas.height)
+  dots.forEach(item => {
+    item.x += item.xa
+    item.y += item.ya
+    item.xa *= item.x > canvas.width || item.x < 0 ? -1 : 1
+    item.ya *= item.y > canvas.height || item.y < 0 ? -1 : 1
+    context.fillStyle = 'rgba(255,218,27,1)'
+    context.fillRect(item.x - 1, item.y - 1, 2, 2)
+    drawParticleLine(item, dots)
+  })
 }
+
+/**
+ * 绘制粒子连接线
+ */
+const drawParticleLine = (dot: any, dots: any[]) => {
+  for (const item of dots) {
+    if (dot === item || item.x === null || item.y === null) continue
+    const xc = dot.x - item.x
+    const yc = dot.y - item.y
+    const distance = Math.sqrt(xc*xc + yc*yc)
+    if (distance < item.max) {
+      const ratio = (item.max - distance) /item.max
+      context.beginPath()
+      context.lineWidth = ratio / 2
+      context.moveTo(dot.x, dot.y)
+      context.lineTo(item.x, item.y)
+      context.strokeStyle = 'rgba(255,218,27,1)'
+      context.stroke()
+    }
+  }
+}
+
+const draw = () => {
+  window.requestAnimationFrame(draw)
+  drawParticle()
+}
+
+onMounted(() => {
+  draw()
+})
 
 </script>
