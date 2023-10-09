d2673dc04e8321da95f6b7de1e2480d9e35d3196
22033757 <mengfanfei@haier.com>
2023-10-09 18:08:52 +0800

    3d地图增加label，icon

--- src/pages/ThreeJs/map.vue
+++ src/pages/ThreeJs/map.vue
@@ -14,10 +14,19 @@ import {
   Scene,
   Shape,
   WebGLRenderer,
-  AxesHelper, Vector3, BufferGeometry, LineBasicMaterial, Line, Box3
+  AxesHelper,
+  Vector3,
+  BufferGeometry,
+  LineBasicMaterial,
+  Line,
+  Box3,
+  Sprite,
+  SpriteMaterial,
+  TextureLoader
 } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
+import { CSS2DObject, CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer'
 import * as d3 from 'd3'
 
 export interface Property {
@@ -50,33 +59,41 @@ const mapRef = ref<HTMLDivElement | null>(null)
 
 // 创建场景
 const scene = new Scene()
+// 设置场景参数
+const setSceneParams = () => {
+  scene.background = new Color(0xffffff)
+}
 
 // 创建相机
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+// 设置相机参数
+const setCameraParams = () => {
+  camera.position.set(0, -1.5, 4)
+}
 
 // 渲染器
 const renderer = new WebGLRenderer({
   // alpha: true, // 透明
   antialias: true // 抗锯齿
 })
-
-// 控制器
-const controls = new OrbitControls(camera, renderer.domElement)
-
-// 设置场景参数
-const setSceneParams = () => {
-  scene.background = new Color(0xffffff)
-}
-// 设置相机参数
-const setCameraParams = () => {
-  camera.position.set(0, -2.8, 6)
-}
 // 设置渲染器参数
 const setRendererParams = () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.setPixelRatio(window.devicePixelRatio)
 }
 
+// CSS2D渲染器
+const css2DRenderer = new CSS2DRenderer()
+// 设置CSS2D渲染器参数
+const setCSS2DRendererParams = () => {
+  css2DRenderer.setSize(window.innerWidth, window.innerHeight)
+  css2DRenderer.domElement.style.position = 'absolute'
+  css2DRenderer.domElement.style.top = '0px'
+  css2DRenderer.domElement.style.pointerEvents = 'none'
+}
+
+// 控制器
+const controls = new OrbitControls(camera, renderer.domElement)
 // 设置控制器参数
 const setControlsParams = () => {
   controls.enableDamping = true
@@ -90,13 +107,13 @@ const addLight = () => {
   scene.add(ambientLight)
   // 平行光
   const light = new DirectionalLight(0xffffff, 0.2)
-  light.position.set(0, 10, 5)
+  light.position.set(0, 5, 8)
   const light2 = light.clone()
-  light2.position.set(0, 10, -5)
+  light2.position.set(0, -5, 8)
   const light3 = light.clone()
-  light3.position.set(5, 10, 0)
+  light3.position.set(5, 0, 8)
   const light4 = light.clone()
-  light4.position.set(-5, 10, 0)
+  light4.position.set(-5, 0, 8)
   // 添加到场景
   scene.add(light, light2, light3, light4)
 }
@@ -127,6 +144,9 @@ const createMap = (mapInfo: IMapInfo) => {
     `).getHex()
     const depth = 0.3
 
+    const label = createLabel(name, point, depth)
+    const icon = createIcon(center, depth)
+
     coordinates.forEach((coordinate) => {
       if (type === 'MultiPolygon') coordinate.forEach(item => fn(item))
       // if (type === 'Polygon') fn(coordinate)
@@ -137,9 +157,9 @@ const createMap = (mapInfo: IMapInfo) => {
         unit.name = name
       }
     })
-    map.add(unit)
-    setCenter(map)
+    map.add(unit, label, icon)
   })
+  setCenter(map)
   return map
 }
 
@@ -154,7 +174,6 @@ const createMesh = (data: [number, number][], color: ColorRepresentation, depth:
       shape.lineTo(x, -y)
     }
   })
-  console.log(shape)
   // 创建挤压缓冲几何体
   const geometry = new ExtrudeGeometry(shape, {
     depth,
@@ -190,13 +209,43 @@ const createLine = (data: [number, number][], depth: number) => {
   downLine.position.z = -0.0001
   return [upLine, downLine]
 }
+// 创建label
+const createLabel = (name: string, point: [number, number], depth: number) => {
+  const div = document.createElement('div')
+  div.style.color = '#fff'
+  div.style.fontSize = '12px'
+  div.style.textShadow = '1px 1px 2px #047cd6'
+  div.textContent = name
+  const label = new CSS2DObject(div)
+  label.scale.set(0.01, 0.01, 0.01)
+  const [x, y] = offsetXY(point) as [number, number]
+  label.position.set(x, -y, depth)
+  return label
+}
+// 创建icon
+const createIcon = (point: [number, number], depth: number) => {
+  const url = new URL('@/assets/imgs/icon.png', import.meta.url).href
+  const map = new TextureLoader().load(url)
+  const icon = new Sprite(new SpriteMaterial({
+    map,
+    transparent: true
+  }))
+  const [x, y] = offsetXY(point) as [number, number]
+  icon.position.set(x, -y, depth + 0.1)
+  icon.scale.set(0.2, 0.2, 0.2)
+  icon.renderOrder = 1
+  return icon
+}
 // 将地图居中
 const setCenter = (map: Object3D) => {
   const box = new Box3().setFromObject(map)
+  // 盒子辅助器
+  // const boxHelper = new Box3Helper(box, new Color(0xffff00))
+  // scene.add(boxHelper)
   const center = box.getCenter(new Vector3())
 
-  map.position.x = center.x
-  map.position.y = center.y
+  map.position.x = map.position.x - center.x
+  map.position.y = map.position.y - center.y
 }
 // 初始化地图
 const initMap = async () => {
@@ -208,11 +257,13 @@ const initMap = async () => {
 // 将画布挂在到DOM上
 const addCanvas = () => {
   mapRef.value?.appendChild(renderer.domElement)
+  mapRef.value?.appendChild(css2DRenderer.domElement)
 }
 
 // 渲染
 const render = () => {
   renderer.render(scene, camera)
+  css2DRenderer.render(scene, camera)
 }
 
 // 动画
@@ -225,6 +276,7 @@ const animate = () => {
 // 监听窗口大小变化
 const handleResize = () => {
   setRendererParams()
+  setCSS2DRendererParams()
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   render()
@@ -255,6 +307,8 @@ onMounted(() => {
   setCameraParams()
   // 初始化渲染器参数
   setRendererParams()
+  // 初始化CSS2D渲染器参数
+  setCSS2DRendererParams()
   // 初始化控制器参数
   setControlsParams()
   // 初始化地图
