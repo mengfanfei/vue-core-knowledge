95b3c3ac29fc588007480d63ea0c9602d0da8bff
mengfanfei <15863882035@163.com>
2023-12-28 17:54:09 +0800

    barCharts更新

--- src/components/chartModules/BarChart/echarts.options.ts
+++ src/components/chartModules/BarChart/echarts.options.ts
@@ -1,16 +1,17 @@
 import { ComposeOption } from 'echarts/core'
-import { BarSeriesOption, DatasetComponentOption } from 'echarts'
+import { BarSeriesOption, DatasetComponentOption, LineSeriesOption } from 'echarts'
 import {
   GridComponentOption,
   LegendComponentOption,
   TooltipComponentOption
 } from 'echarts/components'
 import { OptionDataItemOriginal, OptionDataValue, OptionSourceData } from 'echarts/types/src/util/types'
+import { LineDataItemOption } from 'echarts/types/src/chart/line/LineSeries'
 
 /**
  * 柱状图Option
  */
-export type BarOption = ComposeOption<BarSeriesOption | TooltipComponentOption | GridComponentOption | DatasetComponentOption | LegendComponentOption>
+export type BarOption = ComposeOption<BarSeriesOption | LineSeriesOption | TooltipComponentOption | GridComponentOption | DatasetComponentOption | LegendComponentOption>
 
 export interface BarDataOptions {
   source?: OptionSourceData<OptionDataValue, OptionDataItemOriginal<OptionDataValue>>
@@ -31,13 +32,21 @@ export interface BarDataOptions {
     left?: string | number,
     right?: string | number,
     bottom?: string | number
+  },
+  stack?: string[],
+  yAxisName?: string,
+  unit?: string,
+  lineSource?: LineDataItemOption[]
+  lineStyle?: {
+    color?: string[],
+    unit?: string
   }
 }
 export function getOptions(data: BarDataOptions): BarOption {
 
 
   const getSeries = () => {
-    let series: BarSeriesOption[] = []
+    let series: Array<BarSeriesOption|LineSeriesOption> = []
     if (data.source) {
       for (let i = 1; i < data.source.length; i++) {
         series.push({
@@ -51,7 +60,20 @@ export function getOptions(data: BarDataOptions): BarOption {
           label: {
             show: data.itemLabel,
             position: 'top'
-          }
+          },
+          stack: data.stack?.[i - 1]
+        })
+      }
+    }
+    if (data.lineSource) {
+      for (let i = 0; i < data.lineSource.length; i++) {
+
+        series.push({
+          type: 'line',
+          lineStyle: {
+            color: data.lineStyle?.color?.[i - 1]
+          },
+          data: (data.lineSource as any)[i - 1]
         })
       }
     }
@@ -78,9 +100,14 @@ export function getOptions(data: BarDataOptions): BarOption {
     },
     tooltip: {
       trigger: 'axis',
-      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-      }
+      axisPointer: {  // 坐标轴指示器，坐标轴触发有效
+        type: 'shadow',
+        label: {
+          show: true,
+          backgroundColor: 'rgba(0,0,0,0.7)'
+        },
+      },
+      valueFormatter: (value) => `${value}${data.unit || ''}`
     },
     grid: {
       top: data.gridPosition?.top || 60,
@@ -95,29 +122,58 @@ export function getOptions(data: BarDataOptions): BarOption {
     xAxis: [
       {
         type: 'category',
-        axisLabel: {
+        axisLabel: { // 坐标轴刻度标签的相关设置。
           show: true,
           fontSize: 12,
-          padding: [8, 0, 0, 0],
-          color: 'rgba(89, 89, 89, 1)'
+          // padding: [8, 0, 0, 0],
+          color: 'rgba(89, 89, 89, 1)' // 刻度标签颜色值
         },
-        axisTick: {
+        axisTick: { // 坐标轴刻度相关设置。
           show: true,
           alignWithLabel: true,
-          // lineStyle: {
-          //   color: 'rgba(217, 217, 217, 1)'
-          // }
-        },
-        axisLine: {
           lineStyle: {
-            color: 'rgba(217, 217, 217, 1)'
+            color: 'rgba(217, 217, 217, 1)' // 刻度线颜色值
+          }
+        },
+        axisLine: { // 坐标轴轴线相关设置。
+          lineStyle: {
+            color: 'rgba(217, 217, 217, 1)' // 轴线的颜色
           }
         }
       },
     ],
     yAxis: [
       {
-
+        type: 'value',
+        name:  data.unit ? `${data.yAxisName}(${data.unit})` : data.yAxisName,
+        nameTextStyle: { // 坐标轴名称的相关设置。
+          color: 'rgba(89, 89, 89, 1)',
+          fontSize: 12,
+          padding: [0, data.unit ? 12 : 20, 0, 0]
+        },
+        axisLabel: { // 坐标轴刻度标签的相关设置。
+          show: true,
+          fontSize: 12,
+          color: 'rgba(89, 89, 89, 1)' // 刻度标签颜色值
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: '#ddd', // 分割线颜色值
+            width: 0.5
+          }
+        },
+        alignTicks: true
+      },
+      {
+        show: true,
+        position: 'right',
+        type: 'value',
+        axisLabel: {
+          show: true,
+          formatter: `{value}${data.lineStyle?.unit || ''}`
+        },
+        alignTicks: true
       }
     ],
     series: getSeries()
